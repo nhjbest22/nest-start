d9a53bb5f6f1b3b2e87a5381a594c5ae50b11c2c
seungmin Nam <nhjbest22@g.skku.edu>
2025-06-29 17:44:45 +0900

    feat: extend MoviesController with CRUD operations for movies

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
     return 'This will return all movies';
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return a movie with ID: ${movieId}`;
+  }
+
+  @Post(':id')
+  createMovie(@Param('id') movieId: string) {
+    return `This will create a new movie with Id: ${movieId}`;
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') movieId: string) {
+    return `This will delete a movie with ID: ${movieId}`;
+  }
+
+  @Patch(':id')
+  updateMovie(@Param('id') movieId: string) {
+    return `This will update a movie with ID: ${movieId}`;
   }
 }
