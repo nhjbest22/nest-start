48b458755bd29d75c4b9cef36b5ad52e14f3e141
seungmin Nam <nhjbest22@g.skku.edu>
2025-06-30 17:13:59 +0900

    feat: implement MoviesService and update MoviesController for movie management

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,12 +8,16 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('search')
@@ -22,18 +26,18 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return a movie with ID: ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   createMovie(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
-  deleteMovie(@Param('id') movieId: string) {
-    return `This will delete a movie with ID: ${movieId}`;
+  deleteMovie(@Param('id') movieId: string): boolean {
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new Error(`Movie with ID ${id} not found`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: string): boolean {
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
+    return true;
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+}
